b4f6021c05d37237a59f339a130e558ec074f0b2
rain_hyeon <iiilee0907@naver.com>
2023-12-23 22:51:04 +0900

    11주차

--- 9week/src/App.js
+++ 9week/src/App.js
@@ -2,10 +2,33 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
-import {useState, useRef} from "react";
+import {useRef, useReducer} from "react";
+
+function reducer(state, action) {
+  switch(action.type){
+    case "CREATE":
+      return [action.newItem, ...state];
+    case "UPDATE" : {
+      return state.map((it) => 
+      it.id === action.targetId
+      ?{
+        ...it,
+        isDone: !it.isDone,
+      }
+      : it
+      );
+    }
+
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    
+    default:
+      return state;
+  }
+}
 
 function App() {
-
 
   const mockTodo = [
     {
@@ -17,37 +40,55 @@ function App() {
     {
       id:1,
       isDone: false,
-      content: "React 공부하기",
+      content: "Spring 공부하기",
       createdDate: new Date().getTime(),
     },
     {
       id:2,
       isDone: false,
-      content: "React 공부하기",
+      content: "Django 공부하기",
       createdDate: new Date().getTime(),
     },
 
   ];
   
-  const [todo, setTodo] = useState(mockTodo);
   const idRef = useRef(3);
+  const[todo, dispatch] = useReducer(reducer, mockTodo);
+
+
 
   const onCreate = (content) => {
-    const newItem = {
-      id:0,
-      content,
-      isDone:false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem], ...todo);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current += 1;
+  };
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return(
     <div className='App'>
       <Header />
       <TodoEditor onCreate={onCreate}/>
-      <TodoList />
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   );
 }
