e88f57e90eb8c3836005ddc75fe4ad9601991247
rain_hyeon <iiilee0907@naver.com>
2024-01-01 17:18:50 +0900

    12주차

--- 9week/src/component/TodoList.js
+++ 9week/src/component/TodoList.js
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo, onUpdate, onDelete }) => {
+
+    const analyzeTodo = useMemo(() => {
+        const totalCount = todo.length;
+        const doneCount = todo.filter((it) => it.Done).length;
+        const notDoneCount = totalCount - doneCount;
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount,
+        };
+    }, [todo]);
+
+    const {totalCount, doneCount, notDoneCount} = analyzeTodo;
+
+
     const[search, setSearch] = useState("");
     const onChangeSearch = (e) => {
         setSearch(e.target.value);
@@ -16,6 +31,11 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
     return(
         <div className="TodoList">
             <h4>TodoList 🌱</h4>
+            <div>
+                <div>총개수: {totalCount}</div>
+                <div>완료된 할 일: {doneCount}</div>
+                <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+            </div>
             <input 
             value={search}
             onChange={onChangeSearch}
