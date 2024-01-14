b4d30d50efec2fc0d9a021fd1cffcd578bbc6e07
rain_hyeon <iiilee0907@naver.com>
2024-01-14 21:08:36 +0900

    14주차

---
+++ 14week/index.js
@@ -0,0 +1,20 @@
+//index.js
+
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="index.css" />
+  </head>
+  <body>
+    <div class="toggle"></div>
+    <hr />
+    <h1>0</h1>
+    <button id="increase">+1</button>
+    <button id="decrease">-1</button>
+    <script src="./index.js"></script>
+  </body>
+</html>
